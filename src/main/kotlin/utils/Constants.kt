92267cca51a4f7fd8216fd35311634cd57e31915
serize02 <ernestoserize@gmail.com>
2024-04-26 06:33:15 -0400

    Ktor-News-App

---
+++ src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,5 @@
+package utils
+
+object Constants {
+    val API_KEY = "a30837a9dc5549dc8657c5456cdee970"
+}
---
+++ src/main/kotlin/utils/Utility.kt
@@ -0,0 +1,20 @@
+package utils
+
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import androidx.compose.ui.input.pointer.PointerIcon
+import org.jetbrains.skia.Image
+import java.awt.Cursor
+import java.awt.Desktop
+import java.net.URI
+import java.net.URL
+
+fun handCursor() = PointerIcon(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR))
+
+fun openURL(uri: URI) {
+    val desktop = Desktop.getDesktop()
+    desktop.browse(uri)
+}
+
+fun loadImage(url: String) =
+    Image.makeFromEncoded(URL(url).readBytes())
+        .toComposeImageBitmap()
